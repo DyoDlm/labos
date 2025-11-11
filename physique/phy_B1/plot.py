ffb6eb8c5dec4cd83fd7c3cacae9fc379d48cc34
dyodlm <baccus.delamancha@gmail.com>
2025-11-11 11:54:17 +0100

    ye

--- physique/phy_B1/plot.py
+++ physique/phy_B1/plot.py
@@ -42,21 +42,53 @@ def plot(df, exp: str, name: str) -> str:
     plt.savefig(fileName)
     return fileName
 
+def parse(df, param: str):
+    status = 0
+    new_tab = []
+    array = []
+    for line in df:
+        if line == param:
+            new_tab = []
+            if status == 0:
+                status == 1
+            else:
+                status = 0
+        elif status == 1:
+            new_tab.insert(1, line)
+            print("New line in tab : ", line)
+        if line == "nan":
+            array.insert(1, new_tab)
+            new_tab = []
+            status = 0
+
+#        new_tab.insert(1, line)
+#        print(new_tab)
+    return array
 
 directory = ""
 file = "phy_B1.csv"
 df = pd.read_csv(file, sep=';')
 
-phase=df[df.columns[0]].to_numpy())
-courant=df[df.columns[1]].to_numpy())
-tensionGen=df[df.columns[2]].to_numpy())
-tensionRee=df[df.columns[3]].to_numpy())
-frequence=df[df.columns[4]].to_numpy())
+phase           = df[df.columns[0]].to_numpy()
+courant         = df[df.columns[1]].to_numpy()
+tensionGen      = df[df.columns[2]].to_numpy()
+tensionRee      = df[df.columns[3]].to_numpy()
+frequence       = df[df.columns[4]].to_numpy()
 
-g_phase         = parse(phase)
-g_courant       = parse(courant)
-g_tensionGen    = parse(tensionGen)
-g_tensionRee    = parse(frequence)
+#print("PHASE     : ", phase)
+#print("COURANT   : ", courant)
+#print("TENSION GEN : ", tensionGen)
+#print("TENSION REE : ", tensionRee)
+#print("FREQUENCE : ", frequence)
+
+g_phase         = parse(phase, "Phase phi +/- 0,1 [deg]")
+for i in g_phase:
+    print("New phase : ", i)
+exit()
+g_courant       = parse(courant, "Courant I +/- 10 [mA]")
+g_tensionGen    = parse(tensionGen, "Tension Vgen (pp)+/- Datasheet [V]")
+g_tensionRee    = parse(tensionRee, "Tension Vreel (pp) +/-0.001 [V]")
+g_frequence     = parse(frequence, "frequence [Hz]")
 
 #df = pd.read_excel(directory + file, engine=".csv")#, engine=".ods")
 for i in df[df.columns[0]].to_numpy():
