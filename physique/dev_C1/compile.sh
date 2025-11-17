a86184a851800e3444bceb802d44a1433b6ae984
dyodlm <baccus.delamancha@gmail.com>
2025-11-17 19:10:51 +0100

    need to check the veracity of equations git add .!

--- physique/dev_C1/compile.sh
+++ physique/dev_C1/compile.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-aux="*.log *.aux *.toc *.out"
+aux="*.log *.aux *.toc *.out srcs/*.log srcs/*.aux srcs/*.out srcs/*.toc"
 
 echo Q | pdflatex main.tex
 
 echo Q | pdflatex main.tex
 
 if [ -f "main.pdf" ] ; then
-	mv main.pdf rapport.pdf
+	mv main.pdf rendu.pdf
 fi
 
 rm $aux
 
 clear && echo "Rapport compiled"
+open rendu.pdf
